1c1abb497a1281a97a6856a38aad211cb61a5167
sakib <sakibul706@gmail.com>
2023-06-12 13:18:04 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dream
+# Dream nots site
---
+++ helper/main.js
@@ -0,0 +1,83 @@
+const link = window.location.href.split("/");
+let root_link = ""
+if (link.length > 3) {
+    root_link = link[3]
+}
+const nav_html = `
+<nav class="navbar" id="navbar">
+    <ul>
+        <li>
+            <a href="/${root_link}">
+                <span class="svg_icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16"> <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/> <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/> </svg>
+                </span>
+            </a>
+        </li>
+        <li id="fontPlus"></li>
+        <li id="theme-tgl"></li>
+    </ul>
+</nav>
+`
+document.getElementById("nav_div").innerHTML = nav_html
+
+
+
+
+const sun_svg = `<span class="svg_icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16"> <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/> </svg></span>`
+const moon_svg = `<span class="svg_icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16"> <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/><path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/></svg></span>`
+const Classes = document.body.classList;
+const themeTgl = document.getElementById("theme-tgl")
+
+function getBrowserPreferredTheme() {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    if (mediaQuery.matches) {
+        Classes.add("dark");
+        changeThemeIcon(true);
+    }else {
+        Classes.add("light");
+        changeThemeIcon(false);
+    }
+
+    // return mediaQuery.matches ? "dark" : "light";
+}
+
+// true means dark
+function changeThemeIcon(dark) {
+    if (dark) {
+        themeTgl.innerHTML = moon_svg;
+    } else {
+        themeTgl.innerHTML = sun_svg;
+    }
+}
+getBrowserPreferredTheme()
+
+themeTgl.addEventListener("click", () => {
+    if (Classes.contains("dark")) {
+        Classes.remove("dark");
+        Classes.add("light");
+        changeThemeIcon(false)
+    } else {
+        Classes.add("dark");
+        Classes.remove("light");
+        changeThemeIcon(true);
+    }
+})
+
+
+// nav bar
+let prevScrollpos = window.pageYOffset;
+let navbar = document.getElementById("navbar");
+
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  
+  if (prevScrollpos > currentScrollPos) {
+    navbar.classList.remove("hide-nav");
+    navbar.classList.add("show-nav");
+  } else {
+    navbar.classList.remove("show-nav");
+    navbar.classList.add("hide-nav");
+  }
+  
+  prevScrollpos = currentScrollPos;
+}
